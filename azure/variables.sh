9a00161c690b29f8eeeb2bda8c8153816ec6e3dd
Yashwanth Adunukota <yash@thirdai.com>
2023-11-20 14:19:03 +0530

    lets see

--- azure/variables.sh
+++ azure/variables.sh
@@ -6,7 +6,10 @@ subnet_name=""
 head_node_ipname=""
 admin_name=""
 db_password=""
-jwt_secret=""  # this should be a password-like string 
+jwt_secret=""  # this should be a password-like string
+admin_mail=""
+admin_password=""
+sengrid_key=""
 
 # Can change these variables if desired
 location="centralus"
