48edb34824e4ea9db2c6c1410046a4236be9d77f
Gautam Sharma <gautam@thirdai.com>
2023-12-25 22:36:41 +0530

    removes redundant comments

--- install/upload_license.sh
+++ install/upload_license.sh
@@ -7,15 +7,3 @@ NFS_SHARED_DIR="$(jq -r '.shared_dir' config.json)"
 need_nfs=$(jq -r '.setup_nfs' config.json)
 
 scp $license_path "$USERNAME"@$PUBLIC_NFS_SERVER_IP:$NFS_SHARED_DIR/license
-
-# if [ $need_nfs != true ]; then
-    
-# else
-#     temp_license_location="/home/$USERNAME"
-#     scp $license_path "$USERNAME"@$PUBLIC_NFS_SERVER_IP:$temp_license_location
-
-#     ssh -o StrictHostKeyChecking=no "$USERNAME"@$PUBLIC_NFS_SERVER_IP << EOF
-#     sudo mv $temp_license_location/ndb_enterprise_license.json $NFS_SHARED_DIR/license
-#     sudo setfacl -m u::rwX,g::rwX,o::r-X $NFS_SHARED_DIR/license
-# EOF
-# fi
