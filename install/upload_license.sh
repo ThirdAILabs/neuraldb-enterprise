c4a25495e17149f7dc83bae046b763bad8808a2d
Gautam Sharma <gautam@thirdai.com>
2024-01-02 20:00:57 +0530

    fixes

--- install/upload_license.sh
+++ install/upload_license.sh
@@ -3,7 +3,7 @@
 PUBLIC_NFS_SERVER_IP="$(jq -r '.HEADNODE_IP | .[0]' config.json)"
 USERNAME=$admin_name
 
-scp "$USERNAME"@$PUBLIC_NFS_SERVER_IP:$shared_dir/license
+scp $license_path "$USERNAME"@$PUBLIC_NFS_SERVER_IP:$shared_dir/license
 
 # Files moved from other directory or scp-ed file doesn't inherit required permission set by ACL.
-ssh $USERNAME@$PUBLIC_NFS_SERVER_IP "sudo chmod g+w $shared_dir/license"
+ssh $USERNAME@$PUBLIC_NFS_SERVER_IP "sudo chmod g+w $shared_dir/license/ndb_enterprise_license.json"
