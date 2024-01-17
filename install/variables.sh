19e1b046dcedca68b79bece13eafb7ca8dd6830c
Kartik Sarangmath <kartik@thirdai.com>
2024-01-17 17:56:03 -0600

    reset variables

--- install/variables.sh
+++ install/variables.sh
@@ -3,8 +3,8 @@
 # Should change these variables
 license_path="/Users/kartiksarangmath/Documents/thirdai/neuraldb-enterprise/install/ndb_enterprise_license.json"  # license file must be named ndb_enterprise_license.json
 jwt_secret="1234"  # this should be a password-like string 
-admin_mail="kartik@thirdai.com"
-admin_username="kartik"
+admin_mail="admin@mail.com"
+admin_username="admin"
 admin_password="password"
 genai_key="key-for-generative-ai"
 
