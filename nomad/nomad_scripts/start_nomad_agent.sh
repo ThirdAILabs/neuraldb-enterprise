674a4f9d2e6ad830775d030a337f1a1e6d23f904
Kartik Sarangmath <kartik@thirdai.com>
2024-01-19 00:09:51 -0600

    fix

--- nomad/nomad_scripts/start_nomad_agent.sh
+++ nomad/nomad_scripts/start_nomad_agent.sh
@@ -1,5 +1,5 @@
 # Check for command-line argument
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Usage: $0 <server_enabled> <client_enabled> <node_pool> <node_class> <nomad_server_private_ip>"
     exit 1
 fi
