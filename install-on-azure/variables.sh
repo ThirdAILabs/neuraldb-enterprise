d7fc550586b94dca9f6297be4d649ce81658a358
Kartik Sarangmath <kartik@thirdai.com>
2024-01-23 10:08:47 -0600

    reset config and vars

--- install-on-azure/variables.sh
+++ install-on-azure/variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Should change these variables
-resource_group_name="neuraldb-enterprise-group-kartik-4"
+resource_group_name="neuraldb-enterprise-group"
 vnet_name="neuraldb-enterprise-vnet"
 subnet_name="neuraldb-enterprise-subnet"
 head_node_ipname="neuraldb-enterprise-ip"
@@ -9,5 +9,5 @@ head_node_ipname="neuraldb-enterprise-ip"
 # Can change these variables if desired
 location="centralus"
 vm_type="Standard_B4ms"
-ssh_username="kartik"
+ssh_username="admin"
 vm_count=2  # vm_count must be greater than or equal to 0
--- install/variables.sh
+++ install/variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Should change these variables
-license_path="/Users/kartiksarangmath/Documents/thirdai/neuraldb-enterprise/install/ndb_enterprise_license.json"  # license file must be named ndb_enterprise_license.json
+license_path="/path/to/ndb_enterprise_license.json"  # license file must be named ndb_enterprise_license.json
 jwt_secret="1234"  # this should be a password-like string 
 admin_mail="admin@mail.com"
 admin_username="admin"
 admin_password="password"
-genai_key="key-for-generative-ai"
+genai_key="open-ai-api-key"
 
 # Application autoscaling parameters
 autoscaling_enabled=false
