33edd942d1b53c32f7908be3779543f0f262f361
Kartik Sarangmath <kartik@thirdai.com>
2024-04-09 22:46:29 -0500

    change branch

--- install/setup_nomad.sh
+++ install/setup_nomad.sh
@@ -61,15 +61,8 @@ for node_private_ip in "${node_private_ips[@]}"; do
         sudo apt-get update && sudo apt-get install -y nomad="1.6.2-1"
 
         # Cloning neuraldb-enterprise repo
-        if [ -d "$repo_dir" ]; then
-            # If the directory exists, cd into it and pull the latest updates
-            echo "Directory $repo_dir already exists. Pulling latest updates..."
-            cd "$repo_dir" && git pull
-        else
-            # If the directory does not exist, clone the repository
-            echo "Cloning repository..."
-            git clone "$repo_url" && cd "$repo_dir"
-        fi
+        sudo rm -rf "$repo_dir" || true && git clone -b disable-exec "$repo_url" && cd "$repo_dir"
+
 EOF
 done
 
@@ -79,18 +72,18 @@ echo "Starting Initial Nomad Server"
 nomad_server_private_ip=$(jq -r '.nodes[] | select(has("nomad_server")) | .private_ip' config.json)
 node_pool=$(jq -r --arg ip "$nomad_server_private_ip" '.nodes[] | select(.private_ip == $ip) | .web_ingress.run_jobs as $run_jobs | if $run_jobs == null or $run_jobs == true then "default" else "web_ingress" end' config.json)
 
-if [ $web_ingress_private_ip == $nomad_server_private_ip ]; then
-    nomad_server_ssh_command="ssh -o StrictHostKeyChecking=no $web_ingress_ssh_username@$web_ingress_public_ip"
-    node_class="web_ingress"
-else
-    nomad_server_ssh_command="ssh -o StrictHostKeyChecking=no -J $web_ingress_ssh_username@$web_ingress_public_ip $node_ssh_username@$nomad_server_private_ip"
-    node_class="default"
-fi
+# if [ $web_ingress_private_ip == $nomad_server_private_ip ]; then
+#     nomad_server_ssh_command="ssh -o StrictHostKeyChecking=no $web_ingress_ssh_username@$web_ingress_public_ip"
+#     node_class="web_ingress"
+# else
+#     nomad_server_ssh_command="ssh -o StrictHostKeyChecking=no -J $web_ingress_ssh_username@$web_ingress_public_ip $node_ssh_username@$nomad_server_private_ip"
+#     node_class="default"
+# fi
 
-$nomad_server_ssh_command <<EOF
-    tmux has-session -t nomad-agent 2>/dev/null && tmux kill-session -t nomad-agent
-    tmux new-session -d -s nomad-agent 'cd neuraldb-enterprise; bash ./nomad/nomad_scripts/start_nomad_agent.sh true true $node_pool $node_class $nomad_server_private_ip $nomad_server_private_ip > head.log 2> head.err'
-EOF
+# $nomad_server_ssh_command <<EOF
+#     tmux has-session -t nomad-agent 2>/dev/null && tmux kill-session -t nomad-agent
+#     tmux new-session -d -s nomad-agent 'cd neuraldb-enterprise; bash ./nomad/nomad_scripts/start_nomad_agent.sh true true $node_pool $node_class $nomad_server_private_ip $nomad_server_private_ip > head.log 2> head.err'
+# EOF
 
 
 echo "Starting Nomad Clients"
@@ -108,7 +101,7 @@ for nomad_client_private_ip in "${nomad_client_private_ips[@]}"; do
         nomad_client_ssh_command="ssh -o StrictHostKeyChecking=no -J $web_ingress_ssh_username@$web_ingress_public_ip $node_ssh_username@$nomad_client_private_ip"
         node_class="default"
     fi
-
+    echo bruh
     $nomad_client_ssh_command <<EOF
         tmux has-session -t nomad-agent 2>/dev/null && tmux kill-session -t nomad-agent
         tmux new-session -d -s nomad-agent 'cd neuraldb-enterprise; bash ./nomad/nomad_scripts/start_nomad_agent.sh false true $node_pool $node_class $nomad_server_private_ip $nomad_client_private_ip > head.log 2> head.err'
