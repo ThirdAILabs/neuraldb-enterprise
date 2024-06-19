33a6df23d9a898ecf4ea2721cba3ca4834d055e9
Kartik Sarangmath <kartik@thirdai.com>
2024-06-19 18:29:19 -0500

    revert

--- install/setup.sh
+++ install/setup.sh
@@ -9,7 +9,7 @@ if { [ "$self_hosted_sql_server" == true ] && [ "$external_hosted_sql_server" ==
 fi
 
 # Set up the NFS server on the Head node, and mount the NFS server on each of the clients
-# bash setup_nfs.sh
+bash setup_nfs.sh
 
 # Check if the shared dir collaboration is working properly or not
 ( bash shared_disk_check.sh )
@@ -17,10 +17,10 @@ if [ $? -ne 0 ]; then
     echo "Error: Directory is not being shared among nodes. Ensure that all nodes have access to the shared directory before rerunning this script"
 else
     # Upload the Rag on Rails license to the Nomad cluster
-    # bash upload_license.sh
+    bash upload_license.sh
 
     # # Launch the Nomad Server and Clients
-    # bash setup_nomad.sh
+    bash setup_nomad.sh
 
     # Set up the PostgreSQL server on the Head node, and install the PostgreSQL client on the client nodes
     self_hosted_sql_server=$(jq -r 'any(.nodes[]; has("sql_server"))' config.json)
