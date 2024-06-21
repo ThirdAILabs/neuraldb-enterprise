d58e35a8d5a13e1815b040b1e46f01e5b8a24316
kartikTAI <129414343+kartikTAI@users.noreply.github.com>
2024-06-21 17:15:15 -0500

    Specify custom sql uri (#33)
    * specify sql uri
    
    * minor fixes
    
    * add check for sql_uri and sql_server
    
    * Launch jobs from localhost (#34)
    
    * ssh before launching jobs
    
    * fixes
    
    * revert
    
    * minor comment

--- install/setup.sh
+++ install/setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Exactly one of 'sql_server' and 'sql_uri' must be specified
+self_hosted_sql_server=$(jq -r 'any(.nodes[]; has("sql_server"))' config.json)
+external_hosted_sql_server=$(jq -r 'has("sql_uri")' config.json)
+if { [ "$self_hosted_sql_server" == true ] && [ "$external_hosted_sql_server" == true ]; } || { [ "$self_hosted_sql_server" == false ] && [ "$external_hosted_sql_server" == false ]; }; then
+    echo "Error: Exactly one of sql_server and sql_uri must be supplied in config.json."
+    exit 1
+fi
+
 # Set up the NFS server on the Head node, and mount the NFS server on each of the clients
 bash setup_nfs.sh
 
 # Check if the shared dir collaboration is working properly or not
 ( bash shared_disk_check.sh )
 if [ $? -ne 0 ]; then
-    echo "Directory is not being shared among nodes. Ensure that all nodes have access to the shared directory before rerunning this script"
+    echo "Error: Directory is not being shared among nodes. Ensure that all nodes have access to the shared directory before rerunning this script"
 else
     # Upload the Rag on Rails license to the Nomad cluster
     bash upload_license.sh
@@ -15,7 +23,10 @@ else
     bash setup_nomad.sh
 
     # Set up the PostgreSQL server on the Head node, and install the PostgreSQL client on the client nodes
-    bash setup_postgresql.sh
+    self_hosted_sql_server=$(jq -r 'any(.nodes[]; has("sql_server"))' config.json)
+    if [ "$self_hosted_sql_server" = true ]; then
+        bash setup_postgresql.sh
+    fi
 
     # Now we can launch the Model Bazaar jobs onto our nomad cluster 
     bash launch_nomad_jobs.sh
--- install/update_neuraldb_enterprise.sh
+++ install/update_neuraldb_enterprise.sh
@@ -11,7 +11,15 @@ node_ssh_username=$(jq -r '.ssh_username' config.json)
 
 nomad_server_private_ip=$(jq -r '.nodes[] | select(has("nomad_server")) | .private_ip' config.json)
 
-sql_server_database_password=$(jq -r '.nodes[] | select(has("sql_server")) | .sql_server.database_password' config.json)
+self_hosted_sql_server=$(jq -r 'any(.nodes[]; has("sql_server"))' config.json)
+if [ "$self_hosted_sql_server" = true ]; then
+    sql_server_database_password=$(jq -r '.nodes[] | select(has("sql_server")) | .sql_server.database_password' config.json)
+    sql_server_private_ip=$(jq -r '.nodes[] | select(has("sql_server")) | .private_ip' config.json)
+    sql_uri="postgresql://modelbazaaruser:${sql_server_database_password}@${sql_server_private_ip}:5432/modelbazaar"
+else
+    sql_uri=$(jq -r '.sql_uri' config.json)
+fi
+
 shared_dir=$(jq -r '.nodes[] | select(has("shared_file_system")) | .shared_file_system.shared_dir' config.json)
 
 if [ $web_ingress_private_ip == $nomad_server_private_ip ]; then
@@ -31,4 +39,4 @@ delete_search_ui_url="http://${web_ingress_public_ip}:4646/v1/job/search-ui"
 curl -s -X DELETE -H "X-Nomad-Token: $acl_token" "$delete_model_bazaar_url"
 curl -s -X DELETE -H "X-Nomad-Token: $acl_token" "$delete_search_ui_url"
 
-bash ../nomad/nomad_jobs/submit_nomad_job.sh $web_ingress_public_ip ../nomad/nomad_jobs/model_bazaar_job.hcl.tpl $acl_token DB_PASSWORD=$sql_server_database_password SHARE_DIR=$shared_dir PUBLIC_SERVER_IP=$web_ingress_public_ip PRIVATE_SERVER_IP=$nomad_server_private_ip JWT_SECRET=$jwt_secret ADMIN_USERNAME=$admin_username ADMIN_MAIL=$admin_mail ADMIN_PASSWORD=$admin_password AUTOSCALING_ENABLED=$autoscaling_enabled AUTOSCALER_MAX_COUNT=$autoscaler_max_count GENAI_KEY=$genai_key
+bash ../nomad/nomad_jobs/submit_nomad_job.sh "$nomad_server_ssh_command" ../nomad/nomad_jobs/model_bazaar_job.hcl.tpl $acl_token SQL_URI=$sql_uri SHARE_DIR=$shared_dir PUBLIC_SERVER_IP=$web_ingress_public_ip PRIVATE_SERVER_IP=$nomad_server_private_ip JWT_SECRET=$jwt_secret ADMIN_USERNAME=$admin_username ADMIN_MAIL=$admin_mail ADMIN_PASSWORD=$admin_password AUTOSCALING_ENABLED=$autoscaling_enabled AUTOSCALER_MAX_COUNT=$autoscaler_max_count GENAI_KEY=$genai_key
